dbea8fcb715397f794686e723ec4c65e7c2fc184
LevanSanadiradze <30416376+LevanSanadiradze@users.noreply.github.com>
2017-12-31 01:27:00 +0400

    Update TabbedScrollViewOnTouchListener.java

--- tabbedscrollview/src/main/java/ge/sanapps/tabbedscrollview/TabbedScrollViewOnTouchListener.java
+++ tabbedscrollview/src/main/java/ge/sanapps/tabbedscrollview/TabbedScrollViewOnTouchListener.java
@@ -22,6 +22,7 @@ public abstract class TabbedScrollViewOnTouchListener implements OnTouchListener
                 lastX = event.getX();
                 lastY = event.getY();
                 break;
+            default:
             case MotionEvent.ACTION_CANCEL:
             case MotionEvent.ACTION_UP:
                 if(wasDown)
